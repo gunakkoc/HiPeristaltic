b8d4a78ca5b1bd27cdf11716ce1d98653fec6ce0
Gun Deniz Akkoc <gunakkoc@gmail.com>
2025-12-13 10:59:13 +0100

    Added TMC2209 driver to stm32duino firmware. Needs testing.

--- firmware/STM32G0B1RET6_BIGTREETECH/stm32duino/HiPeristaltic.ino
+++ firmware/STM32G0B1RET6_BIGTREETECH/stm32duino/HiPeristaltic.ino
@@ -20,7 +20,12 @@
 #define SERIAL_INTERBYTE_TIMEOUT_US 500000L
 #define MOTOR_MIN_PULSE_WIDTH_US 3L //1us for A4988, 2us for DRV8825, ~100ns for TMC2208 and TMC2209
 
+#ifndef TMC2209_driver
+#define TMC2209_driver
+#endif
+
 HardwareSerial Serial5(USART5);
+HardwareSerial Serial3(USART3);
 
 const uint32_t SUB_US_DIV = 1;
 const uint32_t SERIAL_INTERBYTE_TIMEOUT = SERIAL_INTERBYTE_TIMEOUT_US * SUB_US_DIV;
@@ -105,6 +110,316 @@ bool m3_enabled_pin_state = true;
 bool m3_dir_pin_state = false;
 uint32_t m3_step_interval = 4000L;
 // ------- END OF MOTOR PINS AND VARIABLES
+
+// ------- START OF TMC2209 VARIABLES
+uint8_t TMC2209_ustep_exp_int_temp = 0;
+uint32_t TMC2209_ustep_exp_bits_temp = 0;
+// ------- END OF TMC2209 VARIABLES
+
+// -------- START OF TMC2209 Driver ------------
+typedef union CONF_GCONF { //n = 10, RW
+    struct {
+        uint32_t I_scale_analog : 1; //def 1
+        uint32_t internal_Rsense : 1;
+        uint32_t en_SpreadCycle : 1; //0 for StealthChop PWM mode voltage control, 1 for SpreadCycle current control. def 0
+        uint32_t shaft : 1;
+        uint32_t index_otpw : 1;
+        uint32_t index_step : 1;
+        uint32_t pdn_disable : 1;
+        uint32_t mstep_reg_select : 1; //set to 1 for UART control of mres, otherwise pin controlled.
+        uint32_t multistep_filt : 1; //def 1
+        uint32_t test_mode : 1; //set 0
+        uint32_t empty : 22;
+    } fields;
+    uint32_t val;
+    uint8_t byte_arr[4];
+} CONF_GCONF_t;
+
+typedef union CONF_GSTAT { //n = 3, RW(C)
+    struct {
+        uint32_t reset : 1; //resetted
+        uint32_t drv_err : 1; //critical error, write 1 to clear
+        uint32_t uv_cp : 1; //overcurrent, no rewriting to clear
+        // uint32_t openload_a : 1;
+        // uint32_t openload_b : 1;
+        // uint32_t short_a : 1;
+        // uint32_t short_b : 1;
+        // uint32_t s2ga : 1;
+        uint32_t empty : 29;
+    } fields;
+    uint32_t val;
+    uint8_t byte_arr[4];
+} CONF_GSTAT_t;
+
+typedef union CONF_CHOPCONF { //n = 32, RW
+    struct {
+        uint32_t toff : 4; //toff0, toff1, toff2, toff3
+        uint32_t hstrt : 3; //hs0, hs1, hs2
+        uint32_t hend : 4; //hend0, hend1, hend2, hend3
+        uint32_t reserved_1 : 4; //0
+        uint32_t tbl : 2; //tbl0, tbl1
+        uint32_t vsense : 1; 
+        uint32_t reserved_2 : 6; //0
+        uint32_t mres : 4; //mres0, mres1, mres2, mres3
+        uint32_t intpol : 1; //def 1
+        uint32_t dedge : 1;
+        uint32_t diss2g : 1;
+        uint32_t diss2vs : 1;
+    } fields;
+    uint32_t val;
+    uint8_t byte_arr[4];
+} CONF_CHOPCONF_t;
+
+typedef union CONF_VACTUAL { // n = 24, W
+//VACTUAL = v[Hz] / ( f CLK [Hz] / 2^24 ) = v[Hz] / 0.715Hz
+    struct {
+        int32_t vactual : 24; //signed +/- 2^23 also with int32 the sign extension is done and uint32 will not do sign extension
+        uint32_t empty : 8;
+    } fields;
+    uint32_t val; //signed +/- 2^23
+    uint8_t byte_arr[4];
+} CONF_VACTUAL_t;
+
+typedef union CONF_MSCNT { // n = 10, R
+    struct {
+        uint32_t mscnt : 10;
+        uint32_t empty : 22;
+    } fields;
+    uint32_t val;
+    uint8_t byte_arr[4];
+} CONF_MSCNT_t;
+
+typedef union CONF_IHOLD_IRUN { //n = 32, W
+    struct {
+        uint32_t ihold : 5; //def 16
+        uint32_t reserved_1 : 3;
+        uint32_t irun : 5; //def 31
+        uint32_t reserved_2 : 3;
+        uint32_t iholddelay : 4; // def 1 but can be 2 as well, not 0
+        uint32_t empty : 12;
+    } fields;
+    uint32_t val;
+    uint8_t byte_arr[4];
+} CONF_IHOLD_IRUN_t;
+
+typedef union CONF_TPOWERDOWN { // n = 8, W
+    struct {
+        uint32_t tpowerdown : 8; //def 20
+        uint32_t empty : 24;
+    } fields;
+    uint32_t val;
+    uint8_t byte_arr[4];
+}  CONF_TPOWERDOWN_t;
+
+typedef union CONF_PWMCONF { // n = 32, RW
+    struct {
+        uint32_t pwm_ofs : 8; //maybe read initial val from PWM_OFS_AUTO
+        uint32_t pwm_grad : 8; //maybe read initial val from PWM_GRAD_AUTO
+        uint32_t pwm_freq : 2;
+        uint32_t pwm_autoscale : 1;
+        uint32_t pwm_autograd : 1;
+        uint32_t freewheel : 2; //0: normal, 1: freewheel, 2: coil short to LS drivers, 3: coil short to HS drivers
+        uint32_t reserved : 2;
+        uint32_t pwm_reg : 4;
+        uint32_t pwm_lim : 4;
+    } fields;
+    uint32_t val;
+    uint8_t byte_arr[4];
+}  CONF_PWMCONF_t;
+
+typedef union CONF_PWMAUTO { // n = 16, R
+    struct {
+        uint32_t pwm_ofs_auto : 8; //might be useful to store
+        uint32_t pwm_grad_auto : 8; //might be useful to store
+        uint32_t empty : 16;
+    } fields;
+    uint32_t val;
+    uint8_t byte_arr[4];
+}  CONF_PWMAUTO_t;
+
+typedef union CONF_TPWMTHRS { // n = 20, W
+    struct {
+        uint32_t tpwmthrs : 20; //if TPWMTHRS < TSTEP then stealthChop is disabled, spreadCycle is enabled. set 999999 on official firmware for klipper, 0 def disables this
+        uint32_t empty : 12;
+    } fields;
+    uint32_t val;
+    uint8_t byte_arr[4];
+} CONF_TPWMTHRS_t;
+
+typedef union CONF_TSTEP { // n = 20, R
+    struct {
+        uint32_t tstep : 20; //time between steps
+        uint32_t empty : 12;
+    } fields;
+    uint32_t val;
+    uint8_t byte_arr[4];
+} CONF_TSTEP_t;
+
+typedef union CONF_TCOOLTHRS { // n = 20, W
+    struct {
+        uint32_t tcoolthrs : 20;
+        uint32_t empty : 12;
+    } fields;
+    uint32_t val;
+    uint8_t byte_arr[4];
+} CONF_TCOOLTHRS_t;
+
+typedef union CONF_DRV_STATUS { // n = 32, R
+    struct {
+        uint32_t otpw : 1;
+        uint32_t ot : 1;
+        uint32_t s2ga : 1;
+        uint32_t s2gb : 1;
+        uint32_t s2vsa : 1;
+        uint32_t s2vsb : 1;
+        uint32_t ola : 1;
+        uint32_t t120 : 1;
+        uint32_t t143 : 1;
+        uint32_t t150 : 1;
+        uint32_t t157 : 1;
+        uint32_t reserved_1 : 4;
+        uint32_t cs_actual : 5;
+        uint32_t reserved_2 : 9;
+        uint32_t stealth : 1;
+        uint32_t stst : 1;
+    } fields;
+    uint32_t val;
+    uint8_t byte_arr[4];
+}  CONF_DRV_STATUS_t;
+
+typedef struct TMC2209_CONF {
+  CONF_GCONF_t GCONF; //rw
+  CONF_GSTAT_t GSTAT; //rw(c)
+  CONF_CHOPCONF_t CHOPCONF; //rw
+  CONF_VACTUAL_t VACTUAL; //w only
+  CONF_MSCNT_t MSCNT; //r only
+  CONF_IHOLD_IRUN_t IHOLD_IRUN; //w only
+  CONF_TPOWERDOWN_t TPOWERDOWN; //w only
+  CONF_PWMCONF_t PWMCONF; //rw
+  CONF_PWMAUTO_t PWMAUTO; //r only
+  CONF_DRV_STATUS_t DRV_STATUS;
+  CONF_TPWMTHRS_t TPWMTHRS; //w only
+  CONF_TSTEP_t TSTEP; //r only
+  CONF_TCOOLTHRS_t TCOOLTHRS; //w only
+  uint8_t addr_motor;
+} TMC2209_CONF_t;
+
+
+//in CHOPCONF
+const static uint8_t TMC2209_MSTEP256 = 0b0000;
+const static uint8_t TMC2209_MSTEP128 = 0b0001;
+const static uint8_t TMC2209_MSTEP64 = 0b0010;
+const static uint8_t TMC2209_MSTEP32 = 0b0011;
+const static uint8_t TMC2209_MSTEP16 = 0b0100;
+const static uint8_t TMC2209_MSTEP8 = 0b0101;
+const static uint8_t TMC2209_MSTEP4 = 0b0110;
+const static uint8_t TMC2209_MSTEP2 = 0b0111;
+const static uint8_t TMC2209_MSTEP1 = 0b1000;
+
+static const uint8_t reg_GCONF = 0x00;
+static const uint8_t reg_GSTAT = 0x01;
+static const uint8_t reg_CHOPCONF = 0x6C;
+static const uint8_t reg_VACTUAL = 0x22;
+static const uint8_t reg_MSCNT = 0x6A;
+static const uint8_t reg_IHOLD_IRUN = 0x10;
+static const uint8_t reg_TPOWERDOWN = 0x11;
+static const uint8_t reg_PWMCONF = 0x70;
+static const uint8_t reg_PWMAUTO = 0x72;
+static const uint8_t reg_TPWMTHRS = 0x13;
+static const uint8_t reg_TSTEP = 0x12;
+static const uint8_t reg_TCOOLTHRS = 0x14;
+static const uint8_t reg_DRV_STATUS = 0x6F;
+
+static const uint8_t TMC2209_SYNC_BYTE = 0x05;
+
+uint8_t TMC2209_write_reg_msg[8];
+
+TMC2209_CONF_t TMC2209_motors[4];
+
+static uint8_t calc_tmc2209_crc_byte(uint8_t* datagram, uint8_t data_len){ //directly copied from TMC2209 documentation
+    uint8_t i,j;
+    uint8_t crc = 0; 
+    uint8_t currentByte;
+    for (i=0; i<data_len; i++) { // Execute for all bytes of a message
+        currentByte = datagram[i]; // Retrieve a byte to be sent from Array
+        for (j=0; j<8; j++) {
+            if ((crc >> 7) ^ (currentByte&0x01)){ // update CRC based result of XOR operation
+                crc = (crc << 1) ^ 0x07;
+            }
+            else {
+                crc = (crc << 1);
+            }
+            currentByte = currentByte >> 1;
+        } // for CRC bit
+    } // for message byte
+    return crc;
+}
+
+void TMC2209_WriteRegister(uint8_t addr, uint8_t reg, uint32_t value) { //addr 0-3 correspond to motor number 1-4
+    TMC2209_write_reg_msg[0] = TMC2209_SYNC_BYTE; // Sync byte
+    TMC2209_write_reg_msg[1] = addr; // Slave address
+    TMC2209_write_reg_msg[2] = reg | 0x80; // Write bit set
+    TMC2209_write_reg_msg[3] = (value >> 24) & 0xFF;
+    TMC2209_write_reg_msg[4] = (value >> 16) & 0xFF;
+    TMC2209_write_reg_msg[5] = (value >> 8) & 0xFF;
+    TMC2209_write_reg_msg[6] = value & 0xFF;
+    TMC2209_write_reg_msg[7] = calc_tmc2209_crc_byte(TMC2209_write_reg_msg, 7);
+    Serial3.write(TMC2209_write_reg_msg, 8);
+    Serial3.flush();
+}
+
+void TMC2209_Init() {
+    for (int i=0; i<4; i++){
+        TMC2209_motors[i].addr_motor = i; //addr 0 for motor1, addr 1 for motor2 and so on.
+
+        TMC2209_motors[i].GSTAT.fields.reset = 0;
+        TMC2209_motors[i].GSTAT.fields.drv_err = 0;
+        TMC2209_motors[i].GSTAT.fields.uv_cp = 0;
+        TMC2209_WriteRegister(TMC2209_motors[i].addr_motor, reg_GSTAT, TMC2209_motors[i].GSTAT.val);
+
+        TMC2209_motors[i].GCONF.val = 0;
+        TMC2209_motors[i].GCONF.fields.I_scale_analog = 1;
+        TMC2209_motors[i].GCONF.fields.internal_Rsense = 1;
+        TMC2209_motors[i].GCONF.fields.en_SpreadCycle = 0; //0: stealthchop, 1: spreadcycle
+        TMC2209_motors[i].GCONF.fields.mstep_reg_select = 1; //mres by uart
+        TMC2209_motors[i].GCONF.fields.shaft = 0; //0: stealthchop, 1: spreadcycle
+        TMC2209_motors[i].GCONF.fields.index_otpw = 1;
+        TMC2209_motors[i].GCONF.fields.index_step = 0;
+        TMC2209_motors[i].GCONF.fields.pdn_disable = 1;
+        TMC2209_motors[i].GCONF.fields.mstep_reg_select = 1;
+        TMC2209_motors[i].GCONF.fields.multistep_filt = 1;
+        TMC2209_motors[i].GCONF.fields.test_mode = 0;
+        TMC2209_WriteRegister(TMC2209_motors[i].addr_motor, reg_GCONF, TMC2209_motors[i].GCONF.val);
+
+        TMC2209_motors[i].PWMCONF.val = 0xC10D0024;
+        TMC2209_motors[i].PWMCONF.fields.freewheel = 2; //short to LS
+        TMC2209_WriteRegister(TMC2209_motors[i].addr_motor, reg_PWMCONF, TMC2209_motors[i].PWMCONF.val);
+
+        TMC2209_motors[i].CHOPCONF.val = 0x10000053;
+        TMC2209_motors[i].CHOPCONF.fields.intpol = 1;
+        TMC2209_motors[i].CHOPCONF.fields.mres = TMC2209_MSTEP1;
+        TMC2209_WriteRegister(TMC2209_motors[i].addr_motor, reg_CHOPCONF, TMC2209_motors[i].CHOPCONF.val);
+
+        TMC2209_motors[i].IHOLD_IRUN.val = 0;
+        TMC2209_motors[i].IHOLD_IRUN.fields.ihold = 0;
+        TMC2209_motors[i].IHOLD_IRUN.fields.iholddelay = 1;
+        TMC2209_motors[i].IHOLD_IRUN.fields.irun = 31;
+        TMC2209_WriteRegister(TMC2209_motors[i].addr_motor, reg_IHOLD_IRUN, TMC2209_motors[i].IHOLD_IRUN.val);
+
+        TMC2209_motors[i].TCOOLTHRS.fields.tcoolthrs = 0; //disable
+        TMC2209_WriteRegister(TMC2209_motors[i].addr_motor, reg_TCOOLTHRS, TMC2209_motors[i].TCOOLTHRS.val);
+
+        TMC2209_motors[i].TPWMTHRS.fields.tpwmthrs = 0; //disable
+        TMC2209_WriteRegister(TMC2209_motors[i].addr_motor, reg_TPWMTHRS, TMC2209_motors[i].TPWMTHRS.val);
+
+        TMC2209_motors[i].TPOWERDOWN.fields.tpowerdown = 255; //power down after ~1sec = (5.6secs/255)*40
+        TMC2209_WriteRegister(TMC2209_motors[i].addr_motor, reg_TPOWERDOWN, TMC2209_motors[i].TPOWERDOWN.val);
+
+        TMC2209_motors[i].VACTUAL.fields.vactual = 0;
+        TMC2209_WriteRegister(TMC2209_motors[i].addr_motor, reg_VACTUAL, TMC2209_motors[i].VACTUAL.val);
+    }
+}
+// -------- END OF TMC2209 Driver ------------
 
 uint32_t tick_now = 0L;
 
@@ -534,71 +849,101 @@ void set_m3_enabled(){
   send_ack();
 }
 
-//variable microstepping support, not supported by default
-//either implement by switching resolution pins or by implementing commands to TMC motor driver via UART
+//variable microstepping is supported through TMC2209 UART for SKR Mini e3 v3
 void get_m0_var_ustep_support(){
-    snd_buffer[1] = false;
+    snd_buffer[1] = true;
     snd_buffer[0] = rcv_buffer[0];
     send_buffer();
 }
 
 void get_m1_var_ustep_support(){
-    snd_buffer[1] = false;
+    snd_buffer[1] = true;
     snd_buffer[0] = rcv_buffer[0];
     send_buffer();
 }
 
 void get_m2_var_ustep_support(){
-    snd_buffer[1] = false;
+    snd_buffer[1] = true;
     snd_buffer[0] = rcv_buffer[0];
     send_buffer();
 }
 
 void get_m3_var_ustep_support(){
-    snd_buffer[1] = false;
+    snd_buffer[1] = true;
     snd_buffer[0] = rcv_buffer[0];
     send_buffer();
 }
 
+// ###################################### Start TMC2209 Commands ######################################
+
+uint8_t TMC2209_usteps_exp_bits_to_int(uint32_t ustep){
+  if (ustep == TMC2209_MSTEP1) {return 0;}
+  if (ustep == TMC2209_MSTEP2) {return 1;}
+  if (ustep == TMC2209_MSTEP4) {return 2;}
+  if (ustep == TMC2209_MSTEP8) {return 3;}
+  if (ustep == TMC2209_MSTEP16) {return 4;}
+  if (ustep == TMC2209_MSTEP32) {return 5;}
+  if (ustep == TMC2209_MSTEP64) {return 6;}
+  if (ustep == TMC2209_MSTEP128) {return 7;}
+  if (ustep == TMC2209_MSTEP256) {return 8;}
+  return 4;
+}
+
+uint8_t TMC2209_usteps_exp_int_to_bits[] = {TMC2209_MSTEP1,TMC2209_MSTEP2,TMC2209_MSTEP4,TMC2209_MSTEP8,TMC2209_MSTEP16,TMC2209_MSTEP32,TMC2209_MSTEP64,TMC2209_MSTEP128,TMC2209_MSTEP256};
+
 void get_m0_usteps_exp(){
-    snd_buffer[1] = m0_usteps_exp;
-    snd_buffer[0] = rcv_buffer[0];
-    send_buffer();
+  TMC2209_ustep_exp_int_temp = TMC2209_usteps_exp_bits_to_int(TMC2209_motors[0].CHOPCONF.fields.mres);
+  snd_buffer[1] = TMC2209_ustep_exp_int_temp;
+  snd_buffer[0] = rcv_buffer[0];
+  send_buffer();
 }
 
 void set_m0_usteps_exp(){
-    err_cmd(); //not implemented
+  TMC2209_motors[0].CHOPCONF.fields.mres = TMC2209_usteps_exp_int_to_bits[rcv_buffer[1]];
+  TMC2209_WriteRegister(TMC2209_motors[0].addr_motor, reg_CHOPCONF, TMC2209_motors[0].CHOPCONF.val);
+  send_ack();
 }
 
 void get_m1_usteps_exp(){
-    snd_buffer[1] = m1_usteps_exp;
-    snd_buffer[0] = rcv_buffer[0];
-    send_buffer();
+  TMC2209_ustep_exp_int_temp = TMC2209_usteps_exp_bits_to_int(TMC2209_motors[1].CHOPCONF.fields.mres);
+  snd_buffer[1] = TMC2209_ustep_exp_int_temp;
+  snd_buffer[0] = rcv_buffer[0];
+  send_buffer();
 }
 
 void set_m1_usteps_exp(){
-    err_cmd(); //not implemented
+  TMC2209_motors[1].CHOPCONF.fields.mres = TMC2209_usteps_exp_int_to_bits[rcv_buffer[1]];
+  TMC2209_WriteRegister(TMC2209_motors[1].addr_motor, reg_CHOPCONF, TMC2209_motors[1].CHOPCONF.val);
+  send_ack();
 }
 
 void get_m2_usteps_exp(){
-    snd_buffer[1] = m2_usteps_exp;
-    snd_buffer[0] = rcv_buffer[0];
-    send_buffer();
+  TMC2209_ustep_exp_int_temp = TMC2209_usteps_exp_bits_to_int(TMC2209_motors[2].CHOPCONF.fields.mres);
+  snd_buffer[1] = TMC2209_ustep_exp_int_temp;
+  snd_buffer[0] = rcv_buffer[0];
+  send_buffer();
 }
 
 void set_m2_usteps_exp(){
-    err_cmd(); //not implemented
+  TMC2209_motors[2].CHOPCONF.fields.mres = TMC2209_usteps_exp_int_to_bits[rcv_buffer[1]];
+  TMC2209_WriteRegister(TMC2209_motors[2].addr_motor, reg_CHOPCONF, TMC2209_motors[2].CHOPCONF.val);
+  send_ack();
 }
 
 void get_m3_usteps_exp(){
-    snd_buffer[1] = m3_usteps_exp;
-    snd_buffer[0] = rcv_buffer[0];
-    send_buffer();
+  TMC2209_ustep_exp_int_temp = TMC2209_usteps_exp_bits_to_int(TMC2209_motors[3].CHOPCONF.fields.mres);
+  snd_buffer[1] = TMC2209_ustep_exp_int_temp;
+  snd_buffer[0] = rcv_buffer[0];
+  send_buffer();
 }
 
 void set_m3_usteps_exp(){
-    err_cmd(); //not implemented
+  TMC2209_motors[3].CHOPCONF.fields.mres = TMC2209_usteps_exp_int_to_bits[rcv_buffer[1]];
+  TMC2209_WriteRegister(TMC2209_motors[3].addr_motor, reg_CHOPCONF, TMC2209_motors[3].CHOPCONF.val);
+  send_ack();
 }
+// ###################################### End TMC2209 Commands #######################################
+
 
 void get_sub_us_divider(){
   * (uint32_t *) &snd_buffer[1] = SUB_US_DIV;
@@ -697,10 +1042,10 @@ bool process_commands() {
   } else if (rcv_byte_cnt == MSG_LEN){ //entire package is received, process
     rcv_byte_cnt = 0;
     if (check_checksum()){
-	  if (rcv_buffer[0] > CMD_COUNT) {
-		err_cmd();
-		return true;
-	  }
+    if (rcv_buffer[0] > CMD_COUNT) {
+    err_cmd();
+    return true;
+    }
       cmd_fnc_lst[rcv_buffer[0]](); //find the corresponding func by first byte as uint8
     } else {
       err_checksum(); //request data again
@@ -820,7 +1165,13 @@ void setup() {
   //Udp.begin(localPort);
   Serial5.setRx(PD2);
   Serial5.setTx(PD3);
-  Serial5.begin(115200, SERIAL_8N1); 
+  Serial5.begin(115200, SERIAL_8N1);
+  Serial3.setTx(PC_10);
+  Serial3.begin(115200);
+
+  
+  // USART3->CR1 &= ~USART_CR1_RE;  // Disble receiver - STM32 specific
+  
   // UCSR0B &= ~_BV(UDRIE0); //disable "USART, Data Register Empty" interrupt, only works on AVR
 
   //-----MOTOR SETUP-------
@@ -875,6 +1226,8 @@ void setup() {
   m3_enabled_pin_state = false;
   digitalWrite(m3_enabled_pin, LOW); //finally enable back the motor
   //-----------------------
+
+  TMC2209_Init();
 
   delay(10);
   tick_now = micros();
